d720c3688625faff3612af44f987a8bf1d2ee3ed
Chris Norton <chnorton@fontis.com.au>
2023-10-30 12:16:10 +1100

    Add insecure overrides for older nodejs versions

--- flake.nix
+++ flake.nix
@@ -6,7 +6,101 @@
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      with nixpkgs.legacyPackages.${system}; {
+      with (import nixpkgs {
+        inherit system;
+        config = {
+          permittedInsecurePackages = [
+            # Dependencies
+            "openssl-1.1.1w"
+            # v16
+            "nodejs-16.20.2"
+            "nodejs-16.20.1"
+            "nodejs-16.20.0"
+            "nodejs-16.19.1"
+            "nodejs-16.19.0"
+            "nodejs-16.18.1"
+            "nodejs-16.18.0"
+            "nodejs-16.17.1"
+            "nodejs-16.17.0"
+            "nodejs-16.16.0"
+            "nodejs-16.15.1"
+            "nodejs-16.15.0"
+            "nodejs-16.14.2"
+            "nodejs-16.14.1"
+            "nodejs-16.14.0"
+            "nodejs-16.13.2"
+            "nodejs-16.13.1"
+            "nodejs-16.13.0"
+            "nodejs-16.12.0"
+            "nodejs-16.11.1"
+            "nodejs-16.11.0"
+            "nodejs-16.10.0"
+            "nodejs-16.9.1"
+            "nodejs-16.9.0"
+            "nodejs-16.8.0"
+            "nodejs-16.7.0"
+            "nodejs-16.6.2"
+            "nodejs-16.6.1"
+            "nodejs-16.6.0"
+            "nodejs-16.5.0"
+            "nodejs-16.4.2"
+            "nodejs-16.4.1"
+            "nodejs-16.4.0"
+            "nodejs-16.3.0"
+            "nodejs-16.2.0"
+            "nodejs-16.1.0"
+            "nodejs-16.0.0"
+            # v14
+            "nodejs-14.21.3"
+            "nodejs-14.21.2"
+            "nodejs-14.21.1"
+            "nodejs-14.21.0"
+            "nodejs-14.20.1"
+            "nodejs-14.20.0"
+            "nodejs-14.19.3"
+            "nodejs-14.19.2"
+            "nodejs-14.19.1"
+            "nodejs-14.19.0"
+            "nodejs-14.18.3"
+            "nodejs-14.18.2"
+            "nodejs-14.18.1"
+            "nodejs-14.18.0"
+            "nodejs-14.17.6"
+            "nodejs-14.17.5"
+            "nodejs-14.17.4"
+            "nodejs-14.17.3"
+            "nodejs-14.17.2"
+            "nodejs-14.17.1"
+            "nodejs-14.17.0"
+            "nodejs-14.16.1"
+            "nodejs-14.16.0"
+            "nodejs-14.15.5"
+            "nodejs-14.15.4"
+            "nodejs-14.15.3"
+            "nodejs-14.15.2"
+            "nodejs-14.15.1"
+            "nodejs-14.15.0"
+            "nodejs-14.14.0"
+            "nodejs-14.13.1"
+            "nodejs-14.13.0"
+            "nodejs-14.12.0"
+            "nodejs-14.11.0"
+            "nodejs-14.10.1"
+            "nodejs-14.10.0"
+            "nodejs-14.9.1"
+            "nodejs-14.9.0"
+            "nodejs-14.8.0"
+            "nodejs-14.7.0"
+            "nodejs-14.6.0"
+            "nodejs-14.5.0"
+            "nodejs-14.4.0"
+            "nodejs-14.3.0"
+            "nodejs-14.2.0"
+            "nodejs-14.1.0"
+            "nodejs-14.0.0"
+          ];
+        };
+      }); {
         packages = let
           buildNodejs20 =
             callPackage "${nixpkgs}/pkgs/development/web/nodejs/nodejs.nix" {
@@ -1142,5 +1236,6 @@
             };
           });
         };
-      });
+      }
+    );
 }
