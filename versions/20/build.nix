4b22cef42a0953c141879b0406c6cf2210a14551
Louis Orleans <louis@orleans.io>
2024-06-25 01:21:46 -0700

    💩 fix ≥ 20.13

--- versions/20/build.nix
+++ versions/20/build.nix
@@ -10,10 +10,22 @@ let
 in
 buildNodejs {
   inherit enableNpm version sha256;
-  patches = [
-    "${nixpkgs}/pkgs/development/web/nodejs/revert-arm64-pointer-auth.patch"
-    "${nixpkgs}/pkgs/development/web/nodejs/disable-darwin-v8-system-instrumentation-node19.patch"
-    "${nixpkgs}/pkgs/development/web/nodejs/bypass-darwin-xcrun-node16.patch"
-    "${nixpkgs}/pkgs/development/web/nodejs/node-npm-build-npm-package-logic.patch"
-  ];
+  patches =
+    nixpkgs.lib.optional (nixpkgs.lib.versionAtLeast "20.13" version) [
+      "${nixpkgs}/pkgs/development/web/nodejs/revert-arm64-pointer-auth.patch"
+    ] ++ [
+      "${nixpkgs}/pkgs/development/web/nodejs/disable-darwin-v8-system-instrumentation-node19.patch"
+      "${nixpkgs}/pkgs/development/web/nodejs/bypass-darwin-xcrun-node16.patch"
+      "${nixpkgs}/pkgs/development/web/nodejs/node-npm-build-npm-package-logic.patch"
+    ] ++ nixpkgs.lib.optional (nixpkgs.lib.versionAtLeast version "20.13") [
+      # "${nixpkgs}/pkgs/development/web/nodejs/use-correct-env-in-tests.patch"
+      (pkgs.fetchpatch2 {
+        url = "https://github.com/nodejs/node/commit/534c122de166cb6464b489f3e6a9a544ceb1c913.patch";
+        hash = "sha256-4q4LFsq4yU1xRwNsM1sJoNVphJCnxaVe2IyL6AeHJ/I=";
+      })
+      (pkgs.fetchpatch2 {
+        url = "https://github.com/nodejs/node/commit/14863e80584e579fd48c55f6373878c821c7ff7e.patch";
+        hash = "sha256-I7Wjc7DE059a/ZyXAvAqEGvDudPjxQqtkBafckHCFzo=";
+      })
+    ];
 }
